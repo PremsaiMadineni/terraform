765855b4cff64318c43d4ac4abd0913e6a2ba02a
Premsai Madineni <135510372+PremsaiMadineni@users.noreply.github.com>
2025-03-25 19:28:22 +0530

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -6,4 +6,5 @@ module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-084568db4383264d4" # replace this
   instance_type_value = "t2.micro"
+  subnet_id_value = "subnet-088bb0089234b04ce". # replace this
 }
