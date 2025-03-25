55cd1861c15eff6575fc33286fad04f76fc30b5d
Premsai Madineni <135510372+PremsaiMadineni@users.noreply.github.com>
2025-03-25 19:27:00 +0530

    Update variables.tf

--- Day-3/modules/ec2_instance/variables.tf
+++ Day-3/modules/ec2_instance/variables.tf
@@ -5,4 +5,7 @@ variable "ami_value" {
 variable "instance_type_value" {
     description = "value for instance_type"
 }
+variable "subnet_id_value" {
+    description = "value for the subnet_id"
+}
 
