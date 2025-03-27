8a20f36fa316c900938f6e5faecd266fdab33a09
Premsai Madineni <135510372+PremsaiMadineni@users.noreply.github.com>
2025-03-27 16:24:35 +0530

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "premsai-s3-demo-xyz"
-    key            = "abhi/terraform.tfstate"
+    key            = "premsai/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
