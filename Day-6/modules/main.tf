fa88d8a6ae6a77b996ea2354f9572fa34326bb36
Premsai Madineni <135510372+PremsaiMadineni@users.noreply.github.com>
2025-03-28 15:19:32 +0530

    Update main.tf

--- Day-6/modules/main.tf
+++ Day-6/modules/main.tf
@@ -1,1 +1,20 @@
 
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+variable "ami" {
+    description = "this AMI for instances"
+  
+}
+variable "instance_type" {
+    description = "this is the instance type for example, : t2.micro"
+  
+}
+
+resource "aws_instance" "example" {
+    ami = var.ami
+    instance_type = var.instance_type
+  
+}
